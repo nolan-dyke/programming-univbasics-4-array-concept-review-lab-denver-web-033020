868146c2aeb66b1d28a105dfac3e4600a9791968
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-02-20 20:18:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,10 @@
 def find_element_index(array, value_to_find)
   counter = 0 
   while array[counter] do
-    
+    if (array[counter] == value_to_find)
+      {
+        return counter
+      }
   end 
 end
 
